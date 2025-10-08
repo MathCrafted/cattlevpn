2c1e9570c0de076a38d53edccdc3006068eb3f7a
MathCrafted <jrhaley324@gmail.com>
2025-10-08 17:43:22 -0500

    rearranged everything lol

--- install.sh
+++ install.sh
@@ -7,8 +7,9 @@ sudo pacman -S openvpn dnsutils easyrsa
 # A section where I should probably copy files
 
 sudo mkdir /etc/openvpn
-sudo mkdir /etc/openvpn/server
 sudo mkdir /etc/openvpn/client
 
-sudo cp ./routes.sh /etc/openvpn/server/routes.sh
-sudo cp ./gen-config.sh /etc/openvpn/server/gen-config.sh
+sudo cp ./remote /etc/openvpn/remote
+sudo cp ./gen-config.sh /etc/openvpn/gen-configs.sh
+sudo cp ./cattlevpn /bin/cattlevpn
+sudo /etc/openvpn/gen-configs.sh
